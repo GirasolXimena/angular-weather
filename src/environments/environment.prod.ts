0502f4c7b0f82f6b6e1cf85e8c9678843109d9dd
Robert Andrade <srandradejr@gmail.com>
2019-10-11 15:30:26 -0500

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,15 @@
 export const environment = {
   production: true,
-  appID: '',
+  appID: '4b61cff30f2394eea7972b3f4b4eaaf5',
   gKey: '',
   config: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: "AIzaSyChKIPtIgAkkVgbOlkkvTjBK0DsMaoaDzY",
+    authDomain: "weather-app-a3d41.firebaseapp.com",
+    databaseURL: "https://weather-app-a3d41.firebaseio.com",
+    projectId: "weather-app-a3d41",
+    storageBucket: "weather-app-a3d41.appspot.com",
+    messagingSenderId: "504650465020",
+    appId: "1:504650465020:web:59a94c7d22918fcadaa179",
+    measurementId: "G-FVQD2YTDHQ"
   }
 };
